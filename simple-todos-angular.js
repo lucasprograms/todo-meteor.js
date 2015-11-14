19360275a0cd925e16be95631877c81b06f6ded7
Lucas McCain <lucasprograms@gmail.com>
2015-11-14 18:19:20 -0500

    add hide-completed option, incomplete count

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -6,15 +6,31 @@ if (Meteor.isClient) {
   angular.module('simple-todos').controller('TodosAppCtrl', ['$scope', '$meteor',
     function ($scope, $meteor) {
 
-      $scope.tasks = $meteor.collection(Tasks);
+      $scope.tasks = $meteor.collection(function () {
+        return Tasks.find($scope.getReactively('query'), {
+          sort: { createdAt: -1 }
+        });
+      });
+
+      $scope.incompleteCount = function () {
+        return Tasks.find({ checked: { $ne: true } }).count();
+      };
 
       $scope.addTask = function(newTask) {
         $scope.tasks.push({
           text:       newTask,
           createdAt:  new Date()
         });
+
       };
 
+      $scope.$watch('hideCompleted', function () {
+        if ($scope.hideCompleted) {
+          $scope.query = { checked: { $ne: true }};
+        } else {
+          $scope.query = {};
+        }
+      });
     }]);
 }
 
