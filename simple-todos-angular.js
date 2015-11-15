683ced68f53edd213061e8c71f029af6fb1d4867
Lucas McCain <lucasprograms@gmail.com>
2015-11-14 19:24:27 -0500

    move database methods off client side

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -1,7 +1,11 @@
 Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
-  angular.module('simple-todos', ['angular-meteor']);
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
+  angular.module('simple-todos', ['angular-meteor', 'accounts.ui']);
 
   angular.module('simple-todos').controller('TodosAppCtrl', ['$scope', '$meteor',
     function ($scope, $meteor) {
@@ -17,11 +21,15 @@ if (Meteor.isClient) {
       };
 
       $scope.addTask = function(newTask) {
-        $scope.tasks.push({
-          text:       newTask,
-          createdAt:  new Date()
-        });
+        $meteor.call('addTask', newTask);
+      };
 
+      $scope.deleteTask = function (task) {
+        $meteor.call('deleteTask', task._id);
+      };
+
+      $scope.setChecked = function (task) {
+        $meteor.call('setChecked', task._id, !task.checked);
       };
 
       $scope.$watch('hideCompleted', function () {
@@ -33,6 +41,29 @@ if (Meteor.isClient) {
       });
     }]);
 }
+
+Meteor.methods({
+  addTask: function (text) {
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.insert({
+      text:       text,
+      createdAt:  new Date(),
+      owner:      Meteor.userId(),
+      username:   Meteor.user().username
+    });
+  },
+
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  },
+
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked }});
+  }
+});
 
 // if (Meteor.isServer) {
 //   Meteor.startup(function () {
